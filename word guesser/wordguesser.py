71ee6d512be8606ba12525ad3fad5675760f4a7d
chrisyap <119669551+slythaa@users.noreply.github.com>
2023-02-02 16:54:25 +1000

    Update wordguesser.py

--- word guesser/wordguesser.py
+++ word guesser/wordguesser.py
@@ -36,20 +36,21 @@ def twoPlayer():
                       f"\n"
                       f"\n"
                       f"\n"
-                      f"This word has", len(host_Word), "letters. Try to guess: ")
+                      f"This word has", len(host_Word), "letters.")
+    print(f"The first letter of this word is: {host_Word[0]}")
 
-    guessed_Word = input()
+    guessed_Word = input("Try to guess: ")
 
     def wrongWord():
         if host_Word != guessed_Word:
-            print(f"The first letter is: {host_Word[0]}")
+            print(f"The second letter is: {host_Word[1]}")
 
         while guessed_Word != host_Word:
 
             second_Guess = input("Guess a second time: ")
 
             if second_Guess != host_Word:
-                print(f"The second letter is: {host_Word[1]}")
+                print(f"The third letter is: {host_Word[2]}")
 
             if second_Guess == host_Word:
                 print(f"You got it right! The word was {host_Word}!")
@@ -58,25 +59,24 @@ def twoPlayer():
             third_Guess = input("Guess a third time: ")
 
             if third_Guess != host_Word:
-                print(f"The third letter is: {host_Word[2]}")
+                print(f"The fourth letter is: {host_Word[3]}")
 
-                if third_Guess == host_Word:
-                    print(f"You got it right! The word was {host_Word}!")
-                    break
+            if third_Guess == host_Word:
+                print(f"You got it right! The word was {host_Word}!")
+                break
 
             fourth_Guess = input("Guess a fourth time: ")
 
             if fourth_Guess != host_Word:
-                print(f"The fourth letter is: {host_Word[3]}")
+                print(f"The fifth letter is: {host_Word[4]}")
 
-                if fourth_Guess == host_Word:
-                    print(f"You got it right! The word was {host_Word}!")
-                    break
+            if fourth_Guess == host_Word:
+                print(f"You got it right! The word was {host_Word}!")
+                break
 
             fifth_Guess = input("Guess a fifth time (LAST CHANCE!): ")
 
             if fifth_Guess != host_Word:
-                print(f"The fifth letter is: {host_Word[4]}")
                 print(f"You ran out of guesses! The word was {host_Word}! Try again next time!")
                 break
 
@@ -101,12 +101,13 @@ def twoPlayer():
 def onePlayer():
 
     with open("1000randomwords.txt") as f:
-        words = [word.strip() for word in f if len(word.strip()) >= 5] #using for statement!!?!?!!?
+        words = [word.strip() for word in f if len(word.strip()) >= 6] #using for statement!!?!?!!?
         computer = (random.choice(words))
 
-    guess = print(f"This word has", len(computer), "letters. Try to guess: ")
+    guess = print(f"This word has", len(computer), "letters.")
+    print(f"The first letter of this word is: {computer[0]}")
 
-    guessed_Word = input()
+    guessed_Word = input(f"Try to guess: ")
 
     while guessed_Word != computer:
         if guessed_Word == computer:
@@ -114,12 +115,12 @@ def onePlayer():
             break
 
         if guessed_Word != computer:
-            print(f"The first letter is: {computer[0]}")
+            print(f"The second letter is: {computer[1]}")
 
             second_Guess = input("Guess a second time: ")
 
             if second_Guess != computer:
-                print(f"The second letter is: {computer[1]}")
+                print(f"The third letter is: {computer[2]}")
 
             if second_Guess == computer:
                 print(f"You got it right! The word was {computer}!")
@@ -128,7 +129,7 @@ def onePlayer():
             third_Guess = input("Guess a third time: ")
 
             if third_Guess != computer:
-                print(f"The third letter is: {computer[2]}")
+                print(f"The fourth letter is: {computer[3]}")
 
             if third_Guess == computer:
                 print(f"You got it right! The word was {computer}!")
@@ -137,7 +138,7 @@ def onePlayer():
             fourth_Guess = input("Guess a fourth time: ")
 
             if fourth_Guess != computer:
-                print(f"The fourth letter is: {computer[3]}")
+                print(f"The fifth letter is: {computer[4]}")
 
             if fourth_Guess == computer:
                 print(f"You got it right! The word was {computer}!")
@@ -146,7 +147,6 @@ def onePlayer():
             fifth_Guess = input("Guess a fifth time (LAST CHANCE!): ")
 
             if fifth_Guess != computer:
-                print(f"The fifth letter is: {computer[4]}")
                 print(f"You ran out of guesses! The word was {computer}! Try again next time!")
                 break
 
@@ -154,29 +154,25 @@ def onePlayer():
                 print(f"You got it right! The word was {computer}!")
                 break
 
-
-
-playing = ""
+playing = None
 
 while playing != "n":
 
-    one_Or_Two = input("One or two players? (1/2): ")
+    oneOrTwo = input("One or two players? (1/2): ")
 
-    if one_Or_Two == "1":
+    if oneOrTwo == "1":
         onePlayer()
-        break
+        playing = input("Again? (y/n): ")
 
-    elif one_Or_Two == "2":
+    elif oneOrTwo == "2":
         twoPlayer()
-        break
+        playing = input("Again? (y/n): ")
 
-playing = input("Again?(y/n): ")
+    else:
+        pass
 
 if playing == "n":
     print("Thanks for playing!")
-
-
-
 
 
 
